123d96abf20a36fc10f425b6e3a885bf92116cc8
Ko Eunbi <srain0723@khu.ac.kr>
2024-10-16 02:04:54 +0900

    Add files via upload

---
+++ iterator/list/iterator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <numeric>
+template <class iterator>
+void Print(iterator begin, iterator end) {
+	for (iterator iter = begin; iter != end; iter++)
+		std::cout << *iter << ", ";
+	std::cout << std::endl;
+}
+int main() {
+	std::vector<int> v1{ 10, 20, 30, 40, 50 };
+	Print(v1.begin(), v1.end());
+	Print(v1.begin(), v1.end() - 2);
+	int sum = std::accumulate(v1.begin(), v1.end(), 0);
+	std::cout << sum << std::endl;
+	sum = std::accumulate(v1.begin(), v1.end() - 2, 0);
+	std::cout << sum << std::endl;
+}
